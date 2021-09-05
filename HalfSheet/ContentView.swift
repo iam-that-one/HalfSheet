d0eb7e61093ea5ee9cdf28bbb6e5475cc249e05f
iam-that-one <iam-that-one@users.noreply.github.com>
2021-09-06 01:35:39 +0300

    Start

---
+++ HalfSheet/ContentView.swift
@@ -0,0 +1,76 @@
+//
+//  ContentView.swift
+//  HalfSheet
+//
+//  Created by Abdullah Alnutayfi on 05/09/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State var showHalfSheet = false
+    var body: some View {
+        ZStack{
+         
+            Color.pink
+                .edgesIgnoringSafeArea(.all)
+          
+            Button(action:{
+                showHalfSheet = true
+            }){
+                VStack{
+                Text("عرض نصف الصفحة")
+                    .frame(width: 300, height: 50)
+                    .background(Color.white)
+                    .foregroundColor(.black)
+                    .cornerRadius(5)
+                    Spacer()
+                    
+                   
+                }
+               
+            }.buttonStyle(PlainButtonStyle())
+            ZStack{
+            if showHalfSheet{
+                VStack{
+                HalfSheet(showHalfSheet: $showHalfSheet)
+                    .cornerRadius(10)
+                
+                }.transition(.move(edge: .bottom))
+                .animation(.spring(response: 1, dampingFraction: 0.7, blendDuration: 1))
+                .offset(y: showHalfSheet ? 400 : 0)
+                
+            }
+        }
+        
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
+struct HalfSheet :View{
+    @Binding var showHalfSheet : Bool
+    var body: some View{
+        ZStack(alignment: .topTrailing){
+            Color.white
+                .edgesIgnoringSafeArea(.all)
+            
+            Button(action:{
+                showHalfSheet = false
+            }){
+                Text("إغلاق")
+                    .foregroundColor(.white)
+                    .padding()
+                    .background(Color.black)
+                    .clipShape(Capsule())
+                    .padding()
+                    
+            }
+        }
+    }
+}
---
+++ HalfSheet/HalfSheetApp.swift
@@ -0,0 +1,17 @@
+//
+//  HalfSheetApp.swift
+//  HalfSheet
+//
+//  Created by Abdullah Alnutayfi on 05/09/2021.
+//
+
+import SwiftUI
+
+@main
+struct HalfSheetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
